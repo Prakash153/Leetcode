5c0e6770f9a745f93c8a4afa2b4d68fa5482d935
Prakash Kumar Singh <Prakashkumarsingh483@gmail.com>
2022-06-20 10:10:08 +0530

    Merge Sort

---
+++ Reverse pair.java
@@ -0,0 +1,62 @@
+# Given an integer array nums, return the number of reverse pairs in the array.
+# A reverse pair is a pair (i, j) where 0 <= i < j < nums.length and nums[i] > 2 * nums[j].
+  
+  // the idea is to use merge Sorting technique. there are three possibiliies : 
+  1. i-j pair is in left half.
+  2. i-j pair is in right half.
+  3. i is in left and j is in right. 
+  
+  so, for each possibility we count the pairs by divide and conquering and in the third possibility before merging the 
+  first and second half we compute the count i-j pairs. 
+    
+    code: 
+
+class Solution {
+    public int reversePairs(int[] nums) {
+        int[] temp = new int[nums.length];
+        return mergeSort(nums,0,nums.length-1,temp);
+    }
+    
+    public int mergeSort(int[] nums, int low, int high, int[] temp) {
+        int count = 0;
+        if(low<high)
+        {
+            int mid = (low+high)/2;
+            count += mergeSort(nums,low,mid,temp);
+            count += mergeSort(nums,mid+1,high,temp);
+            count += merge(nums,low,mid+1,high,temp);
+        }
+        return count;
+    }
+    
+    public int merge(int[] nums, int low, int mid, int high, int[] temp) {
+        int count = 0, x = mid;
+        for(int i=low; i<mid; i++)
+        {
+            while((x<=high) && (nums[i]>2*(long)nums[x]))
+                x++;
+            
+            count += x-mid;
+        }
+        
+        int i = low, j = mid, k = low;
+        while((i<=mid-1) && (j<=high))
+        {
+            if(nums[i]<=nums[j])
+                temp[k++] = nums[i++];
+            else
+                temp[k++] = nums[j++];
+        }
+        
+        while(i<=mid-1)
+            temp[k++] = nums[i++];
+        
+        while(j<=high)
+            temp[k++] = nums[j++];
+        
+        for(i = low; i<=high; i++)
+            nums[i] = temp[i];
+        
+        return count;
+    }
+}
