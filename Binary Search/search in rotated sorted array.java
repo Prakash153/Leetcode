17c13d28d615538eb55b44e39efe2ce60a47af5c
Prakash Kumar Singh <Prakashkumarsingh483@gmail.com>
2022-09-21 16:02:09 +0530

    important

---
+++ Binary Search/search in rotated sorted array.java
@@ -0,0 +1,48 @@
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+  
+  
+  
+  Solution : the idea is used use binary search 
+  1. check if the mid element is in left half or right half. then check if the target is present in that half or not. 
+    
+    left half : -  nums[low] <= nums[mid]
+    right half : - nums[mid] <= nums[high]
+    
+      code : 
+
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0 ; 
+        int high = nums.length - 1 ; 
+        while(low <= high){
+            int mid = low + (high - low)/2 ; 
+            if(nums[mid] == target) 
+                return mid ; 
+            else if(nums[low]<= nums[mid])
+            {
+                if(nums[low] <= target && target  < nums[mid])
+                    high = mid - 1 ; 
+                else
+                    low = mid + 1 ;
+                
+            }
+            else if(nums[high] >=  nums[mid])
+            {
+                if(nums[high] >= target && target > nums[mid])
+                    low = mid + 1 ;
+                else 
+                    high = mid - 1;
+            }
+            
+        }
+        return -1 ;
+    }
+}
+    
