0f4ca042307186af9bdd75642395b4af16c1bdbe
Prakash Kumar Singh <Prakashkumarsingh483@gmail.com>
2022-12-14 22:37:08 +0530

    Important

--- Recursion/Permutations.java
+++ Recursion/Permutations.java
@@ -83,3 +83,37 @@ class Solution {
         return ans ;
     }
 }
+
+
+2nd method : 
+
+swap method : less space complexity 
+
+code: 
+class Solution {
+    public void permutations(int index , int [] nums , List<List<Integer>> ans ){
+        if(index == nums.length-1 ){
+            List<Integer> ds = new ArrayList<>(); 
+            for(int i = 0 ; i < nums.length ; i++){
+                ds.add(nums[i]);
+            }
+            ans.add(new ArrayList<>(ds));
+        }
+
+        for(int i = index ; i < nums.length ; i++){
+            swap(i,index , nums); 
+            permutations(index+1 , nums, ans); 
+            swap(i, index , nums);
+        }
+    }
+    public void swap (int i , int j , int [] nums){
+        int temp = nums[i]; 
+         nums[i] = nums[j]; 
+         nums[j] = temp;
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>(); 
+        permutations(0 , nums , ans); 
+        return ans ;
+    }
+}
