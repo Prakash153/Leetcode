28e7fdae89f22d61de3f2653a52f44a2c9c4bed0
Prakash Kumar Singh <Prakashkumarsingh483@gmail.com>
2023-04-04 17:09:05 +0530

    DFS

---
+++ Graph/200. Number of Islands.java
@@ -0,0 +1,35 @@
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '
+  0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent 
+lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+  
+  
+  DFS solution :
+
+public class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    clearRestOfLand(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+    
+    private void clearRestOfLand(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] == '0') return;
+        
+        grid[i][j] = '0';
+        clearRestOfLand(grid, i+1, j);
+        clearRestOfLand(grid, i-1, j);
+        clearRestOfLand(grid, i, j+1);
+        clearRestOfLand(grid, i, j-1);
+        return;
+    }
+}
