bf7ca788a3d4684532fef8f9b14ea68aad67113f
Prakash Kumar Singh <Prakashkumarsingh483@gmail.com>
2023-04-25 16:17:11 +0530

    Memoization

---
+++ Dynamic Programming/Fibonacci Number.java
@@ -0,0 +1,15 @@
+code: 
+class Solution {
+    public int f(int n,int []dp){
+        if( n <= 1)  return n; 
+        if(dp[n] != -1) return dp[n]; 
+
+        return dp[n] = f(n-1,dp) + f(n-2,dp);
+    }
+    public int fib(int n) {
+        int dp[] = new int[30+1]; 
+        Arrays.fill(dp,-1);
+        return f(n,dp);
+        
+    }
+}
