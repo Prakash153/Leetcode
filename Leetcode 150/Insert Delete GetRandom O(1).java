05cb28d362cc0afbae2162fe0f9f0fc5f09c06ed
Prakash Kumar Singh <Prakashkumarsingh483@gmail.com>
2023-07-06 20:56:12 +0530

    380

---
+++ Leetcode 150/Insert Delete GetRandom O(1).java
@@ -0,0 +1,47 @@
+Implement the RandomizedSet class:
+
+RandomizedSet() Initializes the RandomizedSet object.
+bool insert(int val) Inserts an item val into the set if not present. Returns true if the item was not present, false otherwise.
+bool remove(int val) Removes an item val from the set if present. Returns true if the item was present, false otherwise.
+int getRandom() Returns a random element from the current set of elements (it's guaranteed that at least one 
+  element exists when this method is called). Each element must have the same probability of being returned.
+You must implement the functions of the class such that each function works in average O(1) time complexity.
+
+
+
+code: 
+
+
+class RandomizedSet {
+
+    Set<Integer> s ;
+    public RandomizedSet() {
+        s = new HashSet<>(); 
+    }
+    
+    public boolean insert(int val) {
+        if(s.contains(val))
+        return false;
+        else{
+            s.add(val); 
+            return true;
+        }
+    }
+    
+    public boolean remove(int val) {
+        if(s.contains(val))
+        {
+            s.remove(val);
+            return true;
+        }
+        else 
+        return false;
+    }
+    
+    public int getRandom() {
+        List<Integer> al = new ArrayList<Integer>(s);
+        int r = (int)(Math.random()*s.size()); 
+
+        return al.get(r); 
+    }
+}
